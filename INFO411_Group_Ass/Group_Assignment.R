8d29a0a51e1fbb410613ed17b81cb81db6946bfe
kikoken831 <kennyhu831@gmail.com>
2022-02-20 01:51:46 +0800

    Added dplyr library into group script

--- INFO411_Group_Ass/Group_Assignment.R
+++ INFO411_Group_Ass/Group_Assignment.R
@@ -7,9 +7,10 @@ library(dummies)
 library(ggplot2)
 library(sp)
 library(maptools)
-
+library(dplyr)
 library(reshape2)
 library(rgeos)
+library(Hmisc)
 
 # Colour palette definition
 pretty_palette <- c("#1f77b4", '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2')
